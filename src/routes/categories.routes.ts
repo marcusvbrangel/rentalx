bb57dd23ae123fc21c5fd1d8fb6cd463e94b0016
marcusvbrangel <marcus.vbrangel@gmail.com>
2022-02-28 09:42:54 -0300

    feat: add repository

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { v4 as uuidv4 } from "uuid";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
+
+const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
+
+categoriesRoutes.post("/", (request, response) => {
+
+  const { name, description } = request.body;
+
+
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already Exists!" });
+  }
+
+
+  categoriesRepository.create({ name, description });
+
+  return response.status(201).send();
+
+});
+
+categoriesRoutes.get("/", (request, response) => {
+
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
+
+});
+
+export { categoriesRoutes }
---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
+
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Olá aplicação" });
+});
+
+app.post("/courses", (request, response) => {
+  
+  const { name } = request.body;
+
+  return response.json({ name });
+
+});
+
+app.listen(3333, () => console.log("Server is running!"));
+
+
