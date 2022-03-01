f76735e5c48b9c323f10dce4933d74110b3ffb84
marcusvbrangel <marcus.vbrangel@gmail.com>
2022-03-01 04:28:34 -0300

    feat: add create specification use case

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,20 +1,12 @@
 import { Router } from "express";
-import { SpecificationsRepository } from "../modules/cars/repositories/implementations/SpecificationsRepository";
+import { createSpecificationController } from "../modules/cars/useCases/createSpecification";
 
 const specificationsRoutes = Router();
 
-const specificationsReposytory = new SpecificationsRepository();
-
 specificationsRoutes.post("/", (request, response) => {
 
-  // const { name, description } = request.body;
-
-  // const createSpecificationService = new CreateSpecificationService(specificationsReposytory);
-
-  // createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
-
+  return createSpecificationController.handle(request, response);
+  
 });
 
 export { specificationsRoutes }
